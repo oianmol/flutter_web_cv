0eb72707561499c7d0d9a5f8cc6bb0734e7db221
Anmol Verma <anmol.verma@mutualmobile.com>
2021-03-05 14:43:46 +0530

    fix mobile, web ui

--- lib/contents/user_tabs.dart
+++ lib/contents/user_tabs.dart
@@ -6,40 +6,74 @@ import 'package:url_launcher/url_launcher.dart';
 class UserTabs extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          TabButton(
-              onPressed: () {
-                _launchURL("https://anmolverma.in");
-              },
-              titleText: "WEBSITE"),
-          TabButton(
-              onPressed: () {
-                _launchURL("https://www.linkedin.com/in/anmolverma92/");
-              },
-              titleText: "LINKEDIN"),
-          TabButton(
-              onPressed: () {
-                handleEmailClicked();
-              },
-              titleText: "EMAIL"),
-          TabButton(
-              onPressed: () {
-                launch("tel://+918284866938");
-              },
-              titleText: "CALL"),
-          Spacer()
-        ],
-      ),
-      width: MediaQuery.of(context).size.width,
-    );
+    return LayoutBuilder(builder: (context, constraints) {
+      debugPrint("Max width is ${constraints.maxWidth}");
+      if (constraints.maxWidth > 400) {
+        return Container(
+          margin: EdgeInsets.symmetric(vertical: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [website(), linkedIn(), email(), phone(), Spacer()],
+          ),
+          width: MediaQuery.of(context).size.width,
+        );
+      } else {
+        return Container(
+          margin: EdgeInsets.symmetric(vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  website(),
+                  linkedIn(),
+                  email(),
+                ],
+              ),
+              phone(),
+            ],
+          ),
+          width: MediaQuery.of(context).size.width,
+        );
+      }
+    });
+  }
+
+  TabButton phone() {
+    return TabButton(
+        onPressed: () {
+          launch("tel://+918284866938");
+        },
+        titleText: "CALL");
+  }
+
+  TabButton email() {
+    return TabButton(
+        onPressed: () {
+          handleEmailClicked();
+        },
+        titleText: "EMAIL");
+  }
+
+  TabButton linkedIn() {
+    return TabButton(
+        onPressed: () {
+          _launchURL("https://www.linkedin.com/in/anmolverma92/");
+        },
+        titleText: "LINKEDIN");
+  }
+
+  TabButton website() {
+    return TabButton(
+        onPressed: () {
+          _launchURL("https://anmolverma.in");
+        },
+        titleText: "WEBSITE");
   }
 
   void handleEmailClicked() {
-     final mailtoLink = Mailto(
+    final mailtoLink = Mailto(
       to: ['anmol.verma4@gmail.com'],
       subject: 'This is a test',
       body: 'This is a test',
