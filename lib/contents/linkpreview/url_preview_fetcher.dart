b0085ece396ce8111e78ef537a07cae7d1306bb4
Anmol Verma <anmol.verma@mutualmobile.com>
2021-03-12 10:29:15 +0530

    fix: button tab width

--- lib/contents/linkpreview/url_preview_fetcher.dart
+++ lib/contents/linkpreview/url_preview_fetcher.dart
@@ -3,17 +3,7 @@ import 'package:metadata_fetch/metadata_fetch.dart';
 
 class UrlPreviewFetcher{
   static Future<Metadata> fetch(String linkPreview) async {
-    // Makes a call
-    var response = await http.get(Uri.parse(linkPreview));
-    print(response);
-    // Covert Response to a Document. The utility function `responseToDocument` is provided or you can use own decoder/parser.
-    var document = responseToDocument(response);
-    print(document);
-
-    // get metadata
-    var data = MetadataParser.openGraph(document);
-    print(data);
-    return data;
+    return extract(linkPreview);
   }
 
 }
